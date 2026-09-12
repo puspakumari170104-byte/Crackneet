4dbf9cd58e76f8ba67295116ceb875a80a47f77a
puspakumari170104-byte <puspakumari170104@gmail.com>
2026-09-12 17:11:29 +0530

    Fix Kotlin stdlib duplicate classes in Android release build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,6 +4,11 @@ dependencies {
     implementation("com.android.billingclient:billing:9.1.0")
 }
 
+configurations.all {
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk7")
+    exclude(group = "org.jetbrains.kotlin", module = "kotlin-stdlib-jdk8")
+}
+
 android {
     namespace = "com.crackneet.app"
     compileSdk = 36
